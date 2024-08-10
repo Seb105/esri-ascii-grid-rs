2ff979c82c73440f13271bdcfe3096094ee77984
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2024-08-10 13:25:36 +0800

    Add optional support for ordered-float

--- src/error.rs
+++ src/error.rs
@@ -29,6 +29,10 @@ pub enum Error {
     #[error("Expecting a float: {0}")]
     ParseFloat(#[from] ParseFloatError),
 
+    #[cfg(feature = "ordered-float")]
+    #[error("A parsed float value resulted in a NaN")]
+    FloatIsNan,
+
     #[error("Expecting {0} rows; got {1}")]
     MismatchedRowCount(usize, usize),
 
@@ -40,4 +44,15 @@ pub enum Error {
 
     #[error("The value {0} in {1} cannot be represented as type {2}")]
     TypeCast(String, String, &'static str),
+}
+
+#[cfg(feature = "ordered-float")]
+impl From<ordered_float::ParseNotNanError<ParseFloatError>> for Error {
+    fn from(err: ordered_float::ParseNotNanError<ParseFloatError>) -> Self {
+        use ordered_float::ParseNotNanError as E;
+        match err {
+            E::ParseFloatError(e) => Self::ParseFloat(e),
+            E::IsNaN => Self::FloatIsNan,
+        }
+    }
 }
